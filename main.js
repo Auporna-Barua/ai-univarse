f9f8e89e9e95b9e6db4b96b43f6550fd49be8b1f
Auporna-Barua <120823697+Auporna-Barua@users.noreply.github.com>
2023-03-03 22:34:19 +0600

    popup show when tool button click

--- main.js
+++ main.js
@@ -6,9 +6,9 @@ const toolInfos = (data) => {
   const card = document.getElementById("all-card");
   card.textContent = "";
 
-    if (data.status) {
-       error.innerHTML = "";
-    for (const info of data.data.tools.slice(0,6)) {
+  if (data.status) {
+    error.innerHTML = "";
+    for (const info of data.data.tools.slice(0, 6)) {
       const div = document.createElement("div");
       div.className = "card";
       div.innerHTML = `
@@ -39,9 +39,9 @@ const toolInfos = (data) => {
             <span class="ms-2 cl">${info.published_in} </span class="ml-4">
             </div>
          </div>
-         <button class="btn arrow-btn" onclick="cardDetails('${
+         <button class="btn arrow-btn" onclick="toolsDetails('${
            info.id
-         }')"><i class="fa-solid fa-arrow-right"></i> </button>
+         }')" data-bs-toggle="modal" data-bs-target="#detailsModal"><i class="fa-solid fa-arrow-right"></i> </button>
          
 
 
@@ -50,8 +50,8 @@ const toolInfos = (data) => {
       card.appendChild(div);
     }
   } else {
-       const error = document.getElementById("error");
-       error.innerHTML = `<h2 class= "text-center text-danger">Oops!😥😪😢 There are no tools</h2>`;
+    const error = document.getElementById("error");
+    error.innerHTML = `<h2 class= "text-center text-danger">Oops!😥😪😢 There are no tools</h2>`;
   }
 };
 
@@ -60,4 +60,67 @@ const toolsDetails = (id) => {
   fetch(`https://openapi.programming-hero.com/api/ai/tool/${id}`)
     .then((response) => response.json())
     .then((data) => console.log(data.data));
+  
+  const details = document.getElementById("details");
+  details.innerHTML = `<div class="modal fade" id="detailsModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header">
+        <button type="button" class="btn-close " data-bs-dismiss="modal" aria-label="Close"></button>
+      </div>
+      <div class="modal-body">
+       <div class="tool-details">
+      <div class="tool-left">
+        <h2>
+          ChatGPT is an AI-powered chatbot platform that uses OpenAI's GPT
+          technology to simulate human conversation.
+        </h2>
+        <div class="price">
+          <div class="box">
+            <h3 style="color: #03a30a">$10/ month Basic</h3>
+          </div>
+          <div class="box"><h3 style="color: #f28927">$50/ month Pro</h3></div>
+          <div class="box">
+            <h3 style="color: #eb5757">Contact us Enterprise</h3>
+          </div>
+        </div>
+        <div class="features">
+          <div>
+            <h2>Features</h2>
+
+            <ul class=" ">
+              <li>A list item</li>
+              <li>A list item</li>
+              <li>A list item</li>
+            </ul>
+          </div>
+          <div>
+            <h2>Integrations</h2>
+            <ul class=" ">
+              <li>A list item</li>
+              <li>A list item</li>
+              <li>A list item</li>
+            </ul>
+          </div>
+        </div>
+      </div>
+
+      <div class="tool-right">
+        <div class="card ">
+          <img src="/img/1.jpg" class="card-img-top img-fluid" alt="..." />
+          <div class="card-body text-center">
+            <h5 class="card-title">Card title</h5>
+            <p class="card-text">
+              Some quick example text to build on the card title and make up the
+              bulk of the card's content.
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+      </div>
+    
+    </div>
+  </div>
+</div>`;
 };
